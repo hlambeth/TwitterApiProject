9f98e9bc05813e6c73d8478a47c1a07cc238a28c
Hadley Lambeth <hlambeth@gmail.com>
2015-07-14 11:27:13 -0500

    added winner functionality and style

--- public/app/tweetoff/tweetoff.js
+++ public/app/tweetoff/tweetoff.js
@@ -9,15 +9,6 @@ angular.module('myTwitter.tweetoff', [])
   $scope.oneUrl = "avatar.png";
   $scope.twoUrl = "avatar.png";
 
-  //  $scope.getTweets = function() {
-  //  return $http.post({
-  //    method: 'POST',
-  //    url: '/tweets',
-  //    data: {
-  //      nameOne: $scope.nameOne,
-  //      nameTwo: $scope.nameTwo
-  //    }
-  //  })
 
   $scope.getTweets = function (name) {
     console.log("getTweets Scope, nameOne: " + $scope.nameOne)
@@ -27,14 +18,22 @@ angular.module('myTwitter.tweetoff', [])
     }).success(function(data, status, headers, config) {
     $scope.oneFollowers = data[0].followers_count
     $scope.twoFollowers = data[1].followers_count
-    $scope.oneUrl = data[0].profile_image_url
-    $scope.twoUrl = data[1].profile_image_url
-    console.log(data)
+    // $scope.oneUrl = data[0].profile_image_url
+    // $scope.twoUrl = data[1].profile_image_url
+    var text = /_normal/gi
+    var oneUrl = data[0].profile_image_url
+    var twoUrl = data[1].profile_image_url
+
+    oneUrl = oneUrl.replace(text, "_400x400")
+    twoUrl = twoUrl.replace(text, "_400x400")
+
+    $scope.oneUrl = oneUrl
+    $scope.twoUrl = twoUrl
+    // console.log(data)
     // when the response is available
   }).
   error(function(data, status, headers, config) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
+    alert("That User Wasn't Found")
   });
  } // function
 }) // controller
